5a3e125e3d2bdb7edde61827e98c8e8ada6255e2
EamonBachari <eamonbachari@gmail.com>
2017-06-16 12:14:56 -0500

    Complete CSS styling including hover and input display format.

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("JS Working");
+function trackInput(){
+
+
+  const inputDigit = document.querySelectorAll(".digit");
+  inputDigit.addEventListener("mouseover", highlightButton());
+
+  const html = `
+    <p>${inputDigit}</p>
+  `
+  document.querySelector('#cal-display').insertAdjacentHTML('afterend', html);
+}
+//Special characters complete array element input and increase index.
+//Special characters are stored in array as well.
+//Equals computes contents of array with respect to pemdas.
+//Stores left, right, and operation. On location of * or /, executes operation, stores in right, right becomes left, operation becomes next operatir, right becomes next value, then continue parse.
+function highlightButton(){
+  inputDigit
+}
