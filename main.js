5805678fe467162cbf4496b25f4d3c7c52df02d1
EamonBachari <eamonbachari@gmail.com>
2017-06-16 20:55:11 -0500

    Prevent consecutive operation inputs. Prevent equal execution on incomplete input. git status

--- main.js
+++ main.js
@@ -1,18 +1,73 @@
-function trackInput(){
 
 
-  const inputDigit = document.querySelectorAll(".digit");
-  inputDigit.addEventListener("mouseover", highlightButton());
 
-  const html = `
-    <p>${inputDigit}</p>
-  `
-  document.querySelector('#cal-display').insertAdjacentHTML('afterend', html);
-}
+
+// var calButton = document.querySelectorAll(".button");
+// for(let i = 0; i < calButton.length; ++i){
+//   calButton[i].addEventListener("click", processButtonInput);
+// }
+
+// const html = `
+//   <p>${inputDigit}</p>
+// `
+// document.querySelector('#cal-display').insertAdjacentHTML('afterend', html);
+
 //Special characters complete array element input and increase index.
 //Special characters are stored in array as well.
 //Equals computes contents of array with respect to pemdas.
 //Stores left, right, and operation. On location of * or /, executes operation, stores in right, right becomes left, operation becomes next operatir, right becomes next value, then continue parse.
-function highlightButton(){
-  inputDigit
+// function processButtonInput(){
+//   console.log("Event listener works!");
+//
+// }
+
+var inputArray = [];
+var inputString = "";
+
+function handleDigit(digit){
+  inputString = inputString + digit;
+  console.log(inputString);//---------------------Testing
+  displayInput(digit);
+}
+
+function handleOperation(operation){
+  if(inputString !== ""){
+    inputArray.push(inputString);
+  }
+  if(!((inputArray[inputArray.length-1] === '*') ||(inputArray[inputArray.length-1] === '/') ||(inputArray[inputArray.length-1] === '+') ||(inputArray[inputArray.length-1] === '-'))
+  && (inputArray.length !== 0)){
+    console.log(operation);//---------------------Testing
+    inputArray.push(operation);
+    clearDisplay();
+    inputString = "";
+    console.log(inputArray);//---------------------Testing
+  }
+}
+
+function handleEquals(){
+  if(inputString !== ""){
+    inputArray.push(inputString);
+    console.log(inputArray);//---------------------Testing
+
+  }
+  //Clear and update display input.
+}
+
+function handleClear(){
+  clearDisplay();
+  inputString = "";
+  inputArray = [];
+}
+
+function clearDisplay(){
+  var calDisplay = document.getElementById("cal-display");
+  calDisplay.textContent = "";
+}
+
+
+function displayInput(input){
+  const html = `
+    <p>${input}</p>
+  `
+  document.querySelector('#cal-display').insertAdjacentHTML('beforeend', html);
 }
